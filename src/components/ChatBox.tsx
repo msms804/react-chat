cec9178a99d79888a02fef47ceac560011632347
msms804 <104570214+msms804@users.noreply.github.com>
2024-04-07 21:59:57 +0900

    채팅 db저장, 챗리스트 렌더링

--- src/components/ChatBox.tsx
+++ src/components/ChatBox.tsx
@@ -1,7 +1,16 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 const ChatBox = () => {
-    //여기서 db로 post요청 필요
+    //여기서 db로 post요청 필요,
+    /**
+     * 1. 입력한 메시지 state에 담는다
+     * 2. 그 state변수 태워서 서버로 보낸다
+     * 
+     */
+    const [message, setMessage] = useState('');
+    const [userId, setUserId] = useState(null);
+    const [username, setUsername] = useState(null);
 
     // axios.post("http://localhost:8080/api/chat")
     //     .then(() => {
@@ -10,22 +19,58 @@ const ChatBox = () => {
     //     .catch(() => {
 
     //     })
+    //로그인한 유저 정보가져오기
+    useEffect(() => {
+        axios.get('http://localhost:8080/accessToken', { withCredentials: true })
+            .then((response) => {
+                setUserId(response.data.id)
+                setUsername(response.data.username)
+            })
+            .catch((error: any) => {
+                console.log(error)
+            })
+    }, [])
+
+    const onChangeMessage = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setMessage(e.target.value);
+    }
+    //submit 함수
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const sendDate = new Date();
+        // message, sendDate, userId, username, _id(이건 자동생성인가봄)
+        axios.post('http://localhost:8080/api/chat', { message, sendDate, userId, username })
+            .then((response) => {
+                console.log("메시지 전송 성공", response.data)
+                console.log("전송시간: ", sendDate);//형식 바꿔야
+                console.log("아이디: ", userId)
+                console.log("닉네임: ", username)
+                setMessage('');
+            })
+            .catch((error) => {
+                console.log("메시지 전송 실패", error)
+            })
+
+    }
 
     return (<>
-        <div className="flex mt-auto">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4">
-                <path fillRule="evenodd" d="M2 4a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V4Zm10.5 5.707a.5.5 0 0 0-.146-.353l-1-1a.5.5 0 0 0-.708 0L9.354 9.646a.5.5 0 0 1-.708 0L6.354 7.354a.5.5 0 0 0-.708 0l-2 2a.5.5 0 0 0-.146.353V12a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5V9.707ZM12 5a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z" clipRule="evenodd" />
-            </svg>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4">
-                <path fillRule="evenodd" d="M3 3a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2H3Zm.895 3.458C4.142 6.071 4.38 6 4.5 6s.358.07.605.458a.75.75 0 1 0 1.265-.805C5.933 4.966 5.274 4.5 4.5 4.5s-1.433.466-1.87 1.153C2.195 6.336 2 7.187 2 8s.195 1.664.63 2.347c.437.687 1.096 1.153 1.87 1.153s1.433-.466 1.87-1.153a.75.75 0 0 0 .117-.402V8a.75.75 0 0 0-.75-.75H5a.75.75 0 0 0-.013 1.5v.955C4.785 9.95 4.602 10 4.5 10c-.121 0-.358-.07-.605-.458C3.647 9.15 3.5 8.595 3.5 8c0-.595.147-1.15.395-1.542ZM9 5.25a.75.75 0 0 0-1.5 0v5.5a.75.75 0 0 0 1.5 0v-5.5Zm1 0a.75.75 0 0 1 .75-.75h2.5a.75.75 0 0 1 0 1.5H11.5v1.25h.75a.75.75 0 0 1 0 1.5h-.75v2a.75.75 0 0 1-1.5 0v-5.5Z" clipRule="evenodd" />
-            </svg>
+        <form onSubmit={onSubmit}>
+            <div className="flex mt-auto">
 
-            <input type="text" className="input input-bordered flex-grow h-8" placeholder="채팅치는곳" />
-            <button><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4">
-                <path d="M2.87 2.298a.75.75 0 0 0-.812 1.021L3.39 6.624a1 1 0 0 0 .928.626H8.25a.75.75 0 0 1 0 1.5H4.318a1 1 0 0 0-.927.626l-1.333 3.305a.75.75 0 0 0 .811 1.022 24.89 24.89 0 0 0 11.668-5.115.75.75 0 0 0 0-1.175A24.89 24.89 0 0 0 2.869 2.298Z" />
-            </svg>
-            </button>
-        </div>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4">
+                    <path fillRule="evenodd" d="M2 4a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V4Zm10.5 5.707a.5.5 0 0 0-.146-.353l-1-1a.5.5 0 0 0-.708 0L9.354 9.646a.5.5 0 0 1-.708 0L6.354 7.354a.5.5 0 0 0-.708 0l-2 2a.5.5 0 0 0-.146.353V12a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5V9.707ZM12 5a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z" clipRule="evenodd" />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4">
+                    <path fillRule="evenodd" d="M3 3a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2H3Zm.895 3.458C4.142 6.071 4.38 6 4.5 6s.358.07.605.458a.75.75 0 1 0 1.265-.805C5.933 4.966 5.274 4.5 4.5 4.5s-1.433.466-1.87 1.153C2.195 6.336 2 7.187 2 8s.195 1.664.63 2.347c.437.687 1.096 1.153 1.87 1.153s1.433-.466 1.87-1.153a.75.75 0 0 0 .117-.402V8a.75.75 0 0 0-.75-.75H5a.75.75 0 0 0-.013 1.5v.955C4.785 9.95 4.602 10 4.5 10c-.121 0-.358-.07-.605-.458C3.647 9.15 3.5 8.595 3.5 8c0-.595.147-1.15.395-1.542ZM9 5.25a.75.75 0 0 0-1.5 0v5.5a.75.75 0 0 0 1.5 0v-5.5Zm1 0a.75.75 0 0 1 .75-.75h2.5a.75.75 0 0 1 0 1.5H11.5v1.25h.75a.75.75 0 0 1 0 1.5h-.75v2a.75.75 0 0 1-1.5 0v-5.5Z" clipRule="evenodd" />
+                </svg>
+
+                <input type="text" className="input input-bordered flex-grow h-8" placeholder="채팅치는곳" onChange={onChangeMessage} value={message} />
+                <button type="submit"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4">
+                    <path d="M2.87 2.298a.75.75 0 0 0-.812 1.021L3.39 6.624a1 1 0 0 0 .928.626H8.25a.75.75 0 0 1 0 1.5H4.318a1 1 0 0 0-.927.626l-1.333 3.305a.75.75 0 0 0 .811 1.022 24.89 24.89 0 0 0 11.668-5.115.75.75 0 0 0 0-1.175A24.89 24.89 0 0 0 2.869 2.298Z" />
+                </svg>
+                </button>
+            </div>
+        </form>
     </>)
 }
 export default ChatBox;
--- src/components/ChatList.tsx
+++ src/components/ChatList.tsx
@@ -25,46 +25,6 @@ const ChatList = () => {
         <p className="mb-auto">
             {/* {messages?.map((item: any) => (<div key={item._id}>{item.message}</div>))} */}
             {messages?.map((item: any) => (<Chat message={item} />))}
-            {/** 
-             * 1. 아.. 요걸 chat컴포넌트로 옮겨야할듯.. --> o
-             * 2. 로그인한 사용자명 왼쪽하단에 박기 --> o
-             * 3. 로그인한사람의 채팅 오른쪽 에 오도록 --> o
-             * 4. db스키마 슬슬 확실히 정할것
-             * --------
-             * 5. 채팅창 방 누르면 디테일페이지로 전환되도록 라우팅
-            */}
-            {/* <Chat /> */}
-            <div className="chat chat-start">
-                <div className="chat-image avatar">
-                    <div className="w-10 rounded-full">
-                        <img alt="Tailwind CSS chat bubble component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-                    </div>
-                </div>
-                <div className="chat-header">
-                    Obi-Wan Kenobi
-                    <time className="text-xs opacity-50">12:45</time>
-                </div>
-                <div className="chat-bubble">You were the Chosen One!</div>
-                <div className="chat-footer opacity-50">
-                    Delivered
-                </div>
-            </div>
-            {/** */}
-            <div className="chat chat-end">
-                <div className="chat-image avatar">
-                    <div className="w-10 rounded-full">
-                        <img alt="Tailwind CSS chat bubble component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-                    </div>
-                </div>
-                <div className="chat-header">
-                    Anakin
-                    <time className="text-xs opacity-50">12:46</time>
-                </div>
-                <div className="chat-bubble">I hate you!</div>
-                <div className="chat-footer opacity-50">
-                    Seen at 12:46
-                </div>
-            </div>
         </p>
     </>)
 }
